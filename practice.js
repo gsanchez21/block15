d566bd82cca8b724b249d637644803e9b2554820
gsanchez21 <medina21g@yahoo.com>
2023-04-23 00:34:13 -0400

    Finished guided practice

--- practice.js
+++ practice.js
@@ -1,18 +1,63 @@
-const shoe = {
-    brand: 'Converse',
-    laces: 'White',
-    velcro: 'None',
-    color: 'Green',
-    price: 39.99,
-    activityType: 'basketball'
-} ;
-console.log(shoe.brand, shoe.price)
+// const shoe = {
+//     brand: 'Converse',
+//     laces: 'White',
+//     velcro: 'None',
+//     color: 'Green',
+//     price: 39.99,
+//     activityType: 'basketball'
+// } ;
+// console.log(shoe.brand, shoe.price)
 
-shoe.material = 'cotton';
-console.table(shoe);
+//adding object property 
+// shoe.material = 'cotton';
+// console.table(shoe);
 
-shoe.color = 'Blue';
-console.table(shoe);
+//updating object property value
+// shoe.color = 'Blue';
+// console.table(shoe);
 
-delete shoe.velcro;
-console.table(shoe);
+// delete shoe.velcro;
+// console.table(shoe);
+
+// For in loop to return properties in the object
+// for ( const property in shoe){
+//     console.log(property)
+// }
+//For in loop to return values in the objects
+// for ( const property in shoe){
+//     console.log(shoe[property])
+//}
+
+//methods to collect keys and values into an array
+// console.log(Object.keys(shoe));
+
+// console.log(Object.values(shoe));
+
+//Guided Practice
+const dinner = {
+    cheeseburger: 12,
+    steak: 20,
+    soup: 8,
+    macAndCheese: 14,
+    soupAndsaladCombo: 16
+};
+//console.log(dinner)
+
+//console.log(Object.keys(dinner));
+
+//console.log(Object.values(dinner));
+// let total = 0;
+// for (const meals in dinner){
+//      total += dinner[meals];
+//     }    
+//     console.log(total);
+
+    const prices = Object.values(dinner);
+    let totalCost = 0;
+    
+    for (let i = 0; i < prices.length; i++) {
+        totalCost += prices[i];
+    }
+    
+    console.log(totalCost);
+
